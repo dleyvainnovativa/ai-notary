be9591257900fc662cf83233f4eac8998f9ddff3
Daniel Leyva <caballerodlc@outlook.com>
2026-06-29 23:57:59 -0600

    v1.2.3

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -134,7 +134,7 @@
         <div id="review-form"></div>
         <div class="d-flex justify-content-end mt-3">
             <button class="btn btn-primary" id="review-save">
-                <i class="fa-solid fa-check me-1"></i> Validate &amp; continue
+                <i class="fa-solid fa-check me-1"></i> Exportar TXT
             </button>
         </div>
     </div>
